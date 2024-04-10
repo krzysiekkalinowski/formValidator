917cbfec84511b8837dc04cdb359ad9f42e37ef3
Krzysztof Kalinowski <krzykalinowski@gmail.com>
2024-04-11 00:07:04 +0200

    added tailwind styles

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,3 +1,5 @@
+const colors = require("tailwindcss/colors");
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./src/**/*.{html,js,ts,jsx,tsx}"],
@@ -5,6 +7,21 @@ module.exports = {
     extend: {},
     fontFamily: {
       default: ["Open Sans", "sans-serif"],
+    },
+    width: {
+      "content-width": "400px",
+    },
+    boxShadow: {
+      "form-shadow": "0 2px 10px rgba(0, 0, 0, 0.3)",
+    },
+    colors: {
+      "text-color": "#777",
+      "border-color": "#f0f0f0",
+      white: "#fff",
+      black: "#000",
+      sky: {
+        500: "#0ea5e9",
+      },
     },
   },
   plugins: [],
