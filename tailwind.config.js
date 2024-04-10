3bc10eb3b5ea2aad5d2465eda00f4b1b813c9513
Krzysztof Kalinowski <krzykalinowski@gmail.com>
2024-04-10 16:22:06 +0200

    added HTML markup and initial tailwind styles

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,9 @@ module.exports = {
   content: ["./src/**/*.{html,js,ts,jsx,tsx}"],
   theme: {
     extend: {},
+    fontFamily: {
+      default: ["Open Sans", "sans-serif"],
+    },
   },
   plugins: [],
 };
