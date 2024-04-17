2847aad7250fc7589df7cac926dc5d1ca2431201
Krzysztof Kalinowski <krzykalinowski@gmail.com>
2024-04-17 19:30:31 +0200

    finished dev

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -18,44 +18,60 @@ const showSuccess = (input) => {
   formControl.className = "flex flex-col gap-y-2 formControl--js success";
 };
 
+//Get field name
+const getFieldName = (input) => {
+  return input.id.charAt(0).toUpperCase() + input.id.slice(1);
+};
+
 //Check email valid
-const checkEmailValid = (email) => {
+const checkEmailValid = (input) => {
   const reg =
     /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|.(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-  return reg.test(String(email).toLowerCase());
+  if (reg.test(input.value.trim())) {
+    showSuccess(input);
+  } else {
+    showError(input, `${getFieldName(input)} is not valid`);
+  }
+};
+
+//Check required field
+const checkRequiredField = (inputArr) => {
+  inputArr.forEach(function (input) {
+    if (input.value.trim() === "") {
+      showError(input, `${getFieldName(input)} is required`);
+    } else {
+      showSuccess(input);
+    }
+  });
+};
+
+//Check input length
+const checkLength = (input, min, max) => {
+  if (input.value.length < min) {
+    showError(input, `Minimum length of ${getFieldName(input)} is ${min}`);
+  } else if (input.value.length > max) {
+    showError(input, `Maximum length of ${getFieldName(input)} is ${max}`);
+  } else {
+    showSuccess(input);
+  }
+};
+
+//Check password match
+const checkPasswordMatch = (input1, input2) => {
+  if (input1.value !== input2.value) {
+    showError(input2, "Passwords do not match");
+  } else {
+    showSuccess(input2);
+  }
 };
 
 //Event listeners
 form.addEventListener("submit", function (e) {
   e.preventDefault();
 
-  if (username.value === "") {
-    showError(username, "Username is required");
-  } else {
-    showSuccess(username);
-  }
-
-  if (email.value === "") {
-    showError(email, "Email is required");
-  } else if (!checkEmailValid(email.value)) {
-    showError(email, "Email is not valid");
-  } else {
-    showSuccess(email);
-  }
-
-  if (password.value === "") {
-    showError(password, "Password is required");
-  } else if (password.value !== password2.value) {
-    showError(password, "Both passwords must be the same");
-  } else {
-    showSuccess(password);
-  }
-
-  if (password2.value === "") {
-    showError(password2, "Password confirm is required");
-  } else if (password2.value !== password.value) {
-    showError(password2, "Both passwords must be the same");
-  } else {
-    showSuccess(password2);
-  }
+  checkRequiredField([username, email, password, password2]);
+  checkLength(username, 3, 15);
+  checkLength(password, 6, 25);
+  checkEmailValid(email);
+  checkPasswordMatch(password, password2);
 });
